51e2556cd5426aa12cdb7ac825dabae4f9e86177
Taimoor Arshad <taimoor.arshad43@gmail.com>
2024-10-05 15:26:43 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, redirect, session, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from models import connect_db, db, User, Tweet
+from forms import UserForm, TweetForm
+from sqlalchemy.exc import IntegrityError
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///amznclone"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = "secret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+
+with app.app_context(): # Need this for Flask 3
+    connect_db(app)
+
+toolbar = DebugToolbarExtension(app)
+
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
---
+++ models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+
+bcrypt = Bcrypt()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+    # db.drop_all() # For debugging if we want to start with a new table each time

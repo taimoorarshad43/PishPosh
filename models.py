16ba58a7fd3f42f5765cf69c95190b21b609505e
Taimoor Arshad <taimoor.arshad43@gmail.com>
2024-10-15 13:28:40 -0400

    Adding User Model

--- models.py
+++ models.py
@@ -13,3 +13,19 @@ def connect_db(app):
     db.init_app(app)
     db.create_all()
     # db.drop_all() # For debugging if we want to start with a new table each time
+
+class User(db.Model):
+
+    """User model with username, first name, last name, and hashedpassword"""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    username = db.Column(db.String(50),
+                         nullable = False,
+                         unique = True)
+    
+    firstname = db.Column(db.String(50), nullable = False)
+    lastname = db.Column(db.String(50))

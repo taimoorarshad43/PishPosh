719397e3e8e49f7f6a4be3fd8e5b0e6f885f8e77
Taimoor Arshad <taimoor.arshad43@gmail.com>
2024-10-15 14:16:35 -0400

    Adding sign up route view logic

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from models import connect_db
-# from forms import UserForm, TweetForm
+from models import connect_db, User
+from forms import AddUserForm
 from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///amznclone"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///pishposh"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
-app.config["SECRET_KEY"] = "secret"
+app.config["SECRET_KEY"] = "seekrat"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 
@@ -21,3 +21,22 @@ toolbar = DebugToolbarExtension(app)
 @app.route('/')
 def home_page():
     return render_template('index.html')
+
+@app.route('/signup', methods = ['GET', 'POST'])
+def signup_page():
+
+    userform = AddUserForm()
+
+    if userform.validate_on_submit(): # Handle our POST requests
+        username = userform.username.data
+        password = userform.password.data
+        firstname = userform.firstname.data
+        lastname = userform.lastname.data
+
+        user = User(username = username, password = password, firstname = firstname, lastname = lastname)
+
+        db.session.add(user)
+        db.session.commit()
+
+    else:
+        return render_template('signup.html', form = userform)

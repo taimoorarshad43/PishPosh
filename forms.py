aa9054c13af65239d74c3b3cc52b3cec28eb228e
Taimoor Arshad <taimoor.arshad43@gmail.com>
2024-10-15 13:53:44 -0400

    Adding User form

--- forms.py
+++ forms.py
@@ -1,3 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField
 
+class AddUserForm(FlaskForm):
+
+    username = StringField("Login Name (required)")
+    password = StringField("Password (required)")
+    firstname = StringField("First Name (required)")
+    lastname = StringField("Last Name")
+

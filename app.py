ac54a46990a7187c81c813117a49fcf283d930d7
Taimoor Arshad <taimoor.arshad43@gmail.com>
2024-11-17 16:50:55 -0500

    Fixing issues with pagination

--- app.py
+++ app.py
@@ -34,25 +34,27 @@ toolbar = DebugToolbarExtension(app)
 def home_page():
 
     offset = request.args.get('page', None)                              # We'll set the offset based on whether user selects 'next' or 'previous'
-    pagination = 1
+    pagination = session.get('page', 0)
 
     # Offset will either be "next" or "previous", we'll then increase pagination and use that in our SQLA query
     if not offset:                                           
-        session['page'] = 1
-        pagination = 1
+        session['page'] = 0
+        pagination = 0
     elif offset == 'next':
-        pagination = session.get('page', 1)
+        pagination = session.get('page', 0)
         pagination += 1
         session['page'] = pagination
     else:
-        pagination = session.get('page', 1)
+        pagination = session.get('page', 0)
         pagination -= 1
         session['page'] = pagination
 
-    # .offset().limit().all()
 
     if pagination < 1:  # To prevent negative pages
-        pagination = 1
+        pagination = 0
+        session['page'] = pagination
+
+    pagination = pagination* 20
 
     products = Product.query.offset(pagination).limit(20).all() # Get a bunch of products to display on the homepage. TODO: Randomize order of products listed
 

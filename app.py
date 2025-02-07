88971dc1b12f544913341592ecf343056136aee8
Taimoor Arshad <taimoor.arshad43@gmail.com>
2025-02-07 15:53:34 -0500

    Adding userroutes blueprint

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
-import json
 import os
 import io
 from time import sleep
 
-from flask import Flask, render_template, redirect, session, flash, request, jsonify, url_for
+from flask import Flask, render_template, redirect, session, flash, request, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
-from sqlalchemy.exc import IntegrityError
 from dotenv import load_dotenv
 from PIL import Image
 
-from stripe_payment import create_payment_intent
 from models import db, connect_db, User, Product
-from forms import SignUpForm, LoginForm
 from mistraldescription import getproductdescription, encodeimage, decodeimage
 
 # Blueprint dependencies
@@ -19,6 +15,7 @@ from blueprints.apiroutes import apiroutes
 from blueprints.checkout import productcheckout
 from blueprints.cart import cartroutes
 from blueprints.product import productroutes
+from blueprints.userroutes import userroutes
 
 load_dotenv()                               # Load environmental variables
 
@@ -35,6 +32,7 @@ app.register_blueprint(apiroutes, url_prefix = "/v1")
 app.register_blueprint(productcheckout)
 app.register_blueprint(cartroutes)
 app.register_blueprint(productroutes)
+app.register_blueprint(userroutes)
 
 
 with app.app_context(): # Need this for Flask 3
@@ -158,171 +156,3 @@ def aiconfirm(userid):
     return render_template('aiconfirm.html', image=img_data_decoded, user=user, title=title, description=description)
 
 ##########################################################################################################################################
-
-        
-
-############################################################### User Routes ###############################################################
-
-@app.route('/user/<int:userid>')
-def profile(userid):
-
-    user = User.query.get_or_404(userid)
-
-    userproducts = []
-
-    for product in user.products: # Get all user products to list on page
-        userproducts.append(product)
-
-    return render_template('profile.html', user = user, products = userproducts)
-
-@app.route('/userdetail')
-def userdetail():
-
-    userid = session.get('userid', None)
-
-    if userid:
-
-        user = User.query.get_or_404(session.get('userid', -1))     # Should only be able to get here if you are logged in
-        userproducts = []
-
-        for product in user.products: # Get all user products to list on page
-            userproducts.append(product)
-
-        return render_template('userdetail.html', user = user, products = userproducts)
-    
-    else:
-
-        flash('Please login to view your profile', 'btn-info')
-        return redirect('/')
-
-@app.route('/signup', methods = ['GET', 'POST'])
-def signup():
-
-    signinform = SignUpForm()
-
-    if signinform.validate_on_submit(): # Handles our POST requests
-        username = signinform.username.data
-        password = signinform.password.data
-        firstname = signinform.firstname.data
-        lastname = signinform.lastname.data
-
-        ####################### Validation of user input #################################
-        if len(username) < 4:
-            signinform.username.errors.append("Username must be at least 4 characters long")
-            return render_template('signup.html', form = signinform)
-        if len(password) <= 6:
-            signinform.password.errors.append("Password must be at least 6 characters long")
-            return render_template('signup.html', form = signinform)
-        if len(firstname) == 0:
-            signinform.firstname.errors.append("You need to add your first name")
-            return render_template('signup.html', form = signinform)
-        ##################################################################################
-
-        user = User.hashpassword(username, password, firstname, lastname)
-
-        db.session.add(user)
-        try:                            # Handles the possibility that the username is already taken
-            db.session.commit()
-        except IntegrityError:
-            signinform.username.errors.append("Username already taken")
-            return render_template('signup.html', form = signinform) # Return to GET route of signin
-
-        session['userid'] = user.id
-        session['username'] = user.username
-        session['userfirstname'] = user.firstname
-        session['userlastname']= user.lastname
-
-        flash("Sign Up Successful!", 'btn-success')
-
-        return redirect('/')
-
-    else:                              # Handles our GET requests
-        return render_template('signup.html', form = signinform)
-    
-@app.route('/login', methods = ['GET', 'POST'])
-def login():
-
-    loginform = LoginForm()
-
-    if loginform.validate_on_submit(): # Handles our POST requests
-
-        username = loginform.username.data
-        password = loginform.password.data
-        user = User.authenticate(username, password)
-
-        if user:                       # With valid user redirect to index and add userid (and other attributes) to session object
-            session['userid'] = user.id
-            session['username'] = user.username
-            session['userfirstname'] = user.firstname
-            session['userlastname']= user.lastname
-
-            return redirect('/')
-        else:
-            loginform.username.errors.append('Incorrect Username/Password combination')
-            return render_template('login.html', form=loginform)
-    else:                              # Handles our GET requests
-        return render_template('login.html', form = loginform)
-    
-@app.route('/logout')
-def logout():
-
-    # When you log out, remove userid from session and clear cart from session
-
-    session.pop('userid', None)
-    session.pop('username', None)
-    session.pop('userfirstname', None)
-    session.pop('userlastname', None)
-    session.pop('cart', None)
-
-    # Trying to remove everything from session after logout
-
-    session.clear()
-
-    flash("You are no longer logged in", 'btn-success')
-
-    return redirect('/')
-    
-# TODO: Delete User route - need to test this
-    
-@app.route('/user/<int:userid>/delete')
-def deleteuser(userid):
-
-    User.query.get(userid).delete()
-    db.session.commit()
-
-    # Should delete all products associated with user as well
-
-    # Should remove userid from session as well.
-
-    session.pop['username', None]
-
-    return redirect('/')
-
-################################################################################################################################################
-
-
-
-# ########################################################### Product Routes #####################################################################
-
-# @app.route('/product/<int:productid>')
-# def getproduct(productid):
-
-#     product = Product.query.get_or_404(productid)
-
-#     # to bookmark what product we last visited, so we can redirect back to it
-#     session['lastviewedproduct'] = productid
-
-#     return render_template('productdetail.html', product=product)
-
-# @app.route('/product/<int:productid>/delete')
-# def deleteproduct(productid):
-
-#     Product.query.filter_by(productid=productid).delete()
-#     db.session.commit()
-
-#     userid = session['userid']
-
-#     flash('Product Deleted', 'btn-danger')
-#     return redirect(f'/user/{userid}')
-
-# ################################################################################################################################################
